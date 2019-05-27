b596e653da093e5a122d500d02eb4c4c55945557
Carl Kakisis <Messinias1@gmail.com>
2019-05-27 13:12:58 -0400

    how to factorialize a number recursion and for loop solution

---
+++ factorializeaNumber.js
@@ -0,0 +1,21 @@
+// function factorialize(num) {
+//     if (num < 0 || num === 0) {
+//       return 1
+//     } else {
+//         console.log(num * factorialize(num - 1))
+//       return num * factorialize(num - 1)
+//     }
+//   }
+  
+//   factorialize(5);
+
+  function factorialize(num) {
+      if (num < 0 || num === 0 || num === 1) {
+          return 1
+      }
+      for (let i = num - 1; i >= 1; i--) {
+        num = num * i
+      }
+  }
+
+  factorialize(3);
